257dbb6511b60348bc53a6e677354269f77750ad
ehab <popalsteha20@gmail.com>
2020-09-02 06:05:10 -0700

    Side Session and new bootstrap library added

---
+++ LCO/Areas/Teacher/Controllers/TeacherHomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LCO.Areas.Teacher.Controllers
+{
+    public class TeacherHomeController : Controller
+    {
+        // GET: Teacher/TeacherHome
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
--- LCO/Controllers/HomeController.cs
+++ LCO/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace LCO.Controllers
         {
             return View();
         }
+        
 
         public ActionResult About()
         {
